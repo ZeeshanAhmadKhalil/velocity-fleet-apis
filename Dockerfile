17c858d1059fd0bc1c4aa1aee30b09b09bf16f17
Zeeshan Ahmad Khalil <41861952+ZeeshanAhmadKhalil@users.noreply.github.com>
2023-03-14 18:29:12 +0500

    not using env

--- Dockerfile
+++ Dockerfile
@@ -39,9 +39,6 @@ COPY --chown=node:node . .
 # Run the build command which creates the production bundle
 RUN npm run build
 
-# Set NODE_ENV environment variable
-ENV NODE_ENV production
-
 # Running `npm ci` removes the existing node_modules directory and passing in --only=production ensures that only the production dependencies are installed. This ensures that the node_modules directory is as optimized as possible
 RUN yarn install --only=production && npm cache clean --force
 
--- src/config/keys.ts
+++ src/config/keys.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 
 ConfigModule.forRoot()
 
-const jwtSecret = process.env.JWT_SECRET
-const connectionString = process.env.CONNECTION_STRING
-const mailjetPublic = process.env.MAILJET_PUBLIC
-const mailjetPrivate = process.env.MAILJET_PRIVATE
+const jwtSecret = "0xiammonkeydluffyandiwillbekingofthepirates"
+const connectionString = "mongodb+srv://velocityfleet:12345678Za@cluster0.hpjtbto.mongodb.net/?retryWrites=true&w=majority"
+const mailjetPublic = "84bcd86b3588f75891444037a37d7f3e"
+const mailjetPrivate = "44c0c7afd93c1c0c0d2e6716881ef282"
+
+console.log("jwtSecret===>", jwtSecret)
 
 export {
     jwtSecret,
